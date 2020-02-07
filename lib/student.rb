c4d1f825a15fce2b129ef05584b8f576c92d286f
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-07 22:53:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,11 +30,12 @@ def self.drop_table
   DB[:conn].execute(sql)
 end
 
-def save(name, grade)
+def save
   sql = <<-SQL
-    INSERT INTO students (name, grade) VALUES (?,?)
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
   SQL
-  DB[:conn].execute(sql, name, grade)
+  DB[:conn].execute(sql, self.name, self.grade)
 
 end
 
